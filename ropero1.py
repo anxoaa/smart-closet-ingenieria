e8c79da61d47d0dd898c3779968f5ea080f59efd
anxoaa <anxoccs@gmail.com>
2026-06-04 15:43:54 +0100

    Create ropero1.py

---
+++ ropero1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+camisa = "negra"
+camisa_usos = 2
+camisa_talla = "M"
+camisa_marca = "Zara"
+
+pantalon = "azul"
+pantalon_usos = 1
+pantalon_talla = "42"
+pantalon_marca = "Zara"
+
+zapatos = "negros"
+zapatos_usos = 3
+zapatos_talla = "43"
+zapatos_marca = "Adidas"
+
+print("Camisa:", camisa, "Usos:", camisa_usos, "Talla:", camisa_talla, "Marca:", camisa_marca)
+print("Pantalón:", pantalon, "Usos:", pantalon_usos, "Talla:", pantalon_talla, "Marca:", pantalon_marca)
+print("Zapatos:", zapatos, "Usos:", zapatos_usos, "Talla:", zapatos_talla, "Marca:", zapatos_marca)
